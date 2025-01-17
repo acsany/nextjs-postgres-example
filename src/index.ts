e64ac36afbbca1b3b98d85db61210b050bf8084e
Philipp <filip.axani+github@gmail.com>
2025-01-17 21:12:37 +0100

    Rename the environment variable to POSTGRES_URL

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import { drizzle } from 'drizzle-orm/node-postgres';
 import { eq } from 'drizzle-orm';
 import { usersTable } from './db/schema';
   
-const db = drizzle(process.env.DATABASE_URL!);
+const db = drizzle(process.env.POSTGRES_URL!);
 
 async function main() {
   const user: typeof usersTable.$inferInsert = {
